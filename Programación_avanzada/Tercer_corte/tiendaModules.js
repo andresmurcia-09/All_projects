5f08ab9a24cf2a215dac55841cb947366f92828e
andresmurcia-09 <andresmurcia0904@gmail.com>
2024-11-15 14:30:24 -0500

    Repo

--- Programación_avanzada/Tercer_corte/tiendaModules.js
+++ Programación_avanzada/Tercer_corte/tiendaModules.js
@@ -73,7 +73,7 @@ const Pedidos = {
         // Calcular el total del pedido
         const total = productosPedido.reduce((sum, prod) => sum + prod.precio, 0);
 
-        // Crear el pedido con la información obtenida
+        // Crea el pedido con la información obtenida
         const pedido = {
             id: pedidos.length + 1,
             clienteId,
